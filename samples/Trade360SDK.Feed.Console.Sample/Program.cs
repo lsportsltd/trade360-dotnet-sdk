24c408238fc4d47619d257b60f1893add1c3364c
Oleksandr Mykhailiuk <oleksandr.mykhailiuk@innovecs.com>
2024-04-29 17:41:50 +0300

    MON-1223 add feed lib

--- samples/Trade360SDK.Feed.Console.Sample/Program.cs
+++ samples/Trade360SDK.Feed.Console.Sample/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Configuration;
+using RabbitMQ.Client;
+
+var rmqHost = ConfigurationManager.AppSettings["rmqHost"];
+var rmqUserName = ConfigurationManager.AppSettings["rmqUserName"];
+var rmqPassword = ConfigurationManager.AppSettings["rmqPassword"];
+
+ConnectionFactory connectionFactory = new()
+{
+    HostName = rmqHost,
+    UserName = rmqUserName,
+    Password = rmqPassword,
+    VirtualHost = "StmInPlay",
+    AutomaticRecoveryEnabled = true,
+    RequestedHeartbeat = TimeSpan.FromSeconds(60),
+    NetworkRecoveryInterval = TimeSpan.FromSeconds(1),
+};
+
+using var connection = connectionFactory.CreateConnection();
+
+Console.WriteLine("connection status {0}", connection.IsOpen);
+
+connection.Close();

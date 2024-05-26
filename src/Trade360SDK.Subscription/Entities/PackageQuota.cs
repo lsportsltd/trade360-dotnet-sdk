b90ff5cf08624c1522b295104a47ca72f5c18890
OleksandrMykhailiuk <111273412+OleksandrMykhailiuk@users.noreply.github.com>
2024-05-26 23:56:49 +0300

    MON-1246 get schedule (#5)
    Co-authored-by: Oleksandr Mykhailiuk <oleksandr.mykhailiuk@innovecs.com>

---
+++ src/Trade360SDK.Subscription/Entities/PackageQuota.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Trade360SDK.Subscription.Entities
+{
+    public class PackageQuota
+    {
+        public int CreditRemaining { get; set; }
+        public int CreditLimit { get; set; }
+        public int UsedCredit { get; set; }
+        public DateTime CurrentPeriodStartDate { get; set; }
+        public DateTime CurrentPeriodEndDate { get; set; }
+    }
+}

50e2dc72cba3902bbd695b1d8d3513e46ecfe501
Oleksandr Mykhailiuk <oleksandr.mykhailiuk@innovecs.com>
2024-05-28 01:17:06 +0300

    MON-1238 get leagues

---
+++ src/Trade360SDK.Mapping/Entities/LeaguesCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Trade360SDK.Mapping.Entities
+{
+    internal class LeaguesCollection
+    {
+        public IEnumerable<League>? Leagues { get; set; }
+    }
+}
---
+++ src/Trade360SDK.Mapping/Models/LeaguesRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Trade360SDK.Common.Models;
+using Trade360SDK.Mapping.Enums;
+
+namespace Trade360SDK.Mapping.Models
+{
+    internal class LeaguesRequest : Request
+    {
+        public IEnumerable<int> SportIds { get; }
+        public IEnumerable<int> LocationIds { get; }
+        public SubscriptionStatus SubscriptionStatus { get; }
+
+        public LeaguesRequest(
+            IEnumerable<int> sportIds,
+            IEnumerable<int> locationIds,
+            SubscriptionStatus subscriptionStatus)
+        {
+            SportIds = sportIds;
+            LocationIds = locationIds;
+            SubscriptionStatus = subscriptionStatus;
+        }
+    }
+}

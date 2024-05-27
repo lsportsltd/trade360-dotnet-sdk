af053dc0259b70775cfaeec1ac68e3b5e7dadfc4
Oleksandr Mykhailiuk <oleksandr.mykhailiuk@innovecs.com>
2024-05-28 01:05:53 +0300

    MON-1237 get locations

---
+++ src/Trade360SDK.Mapping/Entities/LocationsCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Trade360SDK.Mapping.Entities
+{
+    internal class LocationsCollection
+    {
+        public IEnumerable<Location>? Locations { get; set; }
+    }
+}

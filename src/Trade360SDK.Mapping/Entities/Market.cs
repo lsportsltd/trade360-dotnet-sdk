cd3f5e6172406af6324f0c9f1ec9f7959e0cbafc
Oleksandr Mykhailiuk <oleksandr.mykhailiuk@innovecs.com>
2024-06-11 01:44:13 +0300

    MON-1239 get markets

---
+++ src/Trade360SDK.Mapping/Entities/Market.cs
@@ -0,0 +1,9 @@
+﻿namespace Trade360SDK.Mapping.Entities
+{
+    public class Market
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsSettleable { get; set; }
+    }
+}

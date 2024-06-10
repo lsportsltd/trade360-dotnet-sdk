cea151d342367bd367276cf71fce15167f81204b
OleksandrMykhailiuk <111273412+OleksandrMykhailiuk@users.noreply.github.com>
2024-06-11 01:43:20 +0300

    MON-1239 get markets (#14)
    Co-authored-by: Oleksandr Mykhailiuk <oleksandr.mykhailiuk@innovecs.com>

---
+++ src/Trade360SDK.Mapping/Entities/MarketsCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Trade360SDK.Mapping.Entities
+{
+    internal class MarketsCollection
+    {
+        public IEnumerable<Market>? Markets { get; set; }
+    }
+}

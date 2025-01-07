cd95baf3a1ef50a78bc43a5ce4fdf45459968dba
PavelTemno <107048183+PavelTemno@users.noreply.github.com>
2025-01-07 15:28:47 +0200

    Add ProviderMarkets, change bets prices to int, restructure of bet class

---
+++ src/Trade360SDK.Common.Entities/Entities/Markets/BaseBet.cs
@@ -0,0 +1,34 @@
+using System;
+using Trade360SDK.Common.Entities.Enums;
+
+namespace Trade360SDK.Common.Entities.Markets
+{
+    public class BaseBet
+    {
+        public long Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Line { get; set; }
+
+        public string? BaseLine { get; set; }
+
+        public BetStatus Status { get; set; }
+
+        public double? StartPrice { get; set; }
+
+        public double? Price { get; set; }
+        
+        public SettlementType? Settlement { get; set; }
+        
+        public int? SuspensionReason { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
+        public double Probability { get; set; }
+
+        public int? ParticipantId { get; set; }
+
+        public string? PlayerName { get; set; }
+    }
+}

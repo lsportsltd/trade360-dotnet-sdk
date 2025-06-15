3f0d554536ead2aa126629e1fcb1631dc0c3df5b
shirbenishay <shir.b@lsports.eu>
2025-06-15 19:04:52 +0300

    Update src/Trade360SDK.CustomersApi/SubscriptionHttpClient.cs
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/Trade360SDK.CustomersApi/SubscriptionHttpClient.cs
+++ src/Trade360SDK.CustomersApi/SubscriptionHttpClient.cs
@@ -29,7 +29,7 @@ namespace Trade360SDK.CustomersApi
 
         public async Task<FixtureScheduleCollectionResponse> GetInplayFixtureSchedule(GetFixtureScheduleRequestDto requestDto, CancellationToken cancellationToken)
         {
-            if (requestDto == null) throw new ArgumentNullException(nameof(requestDto));
+            if (requestDto == null) { throw new ArgumentNullException(nameof(requestDto)); }
             var request = _mapper.Map<GetFixtureScheduleRequest>(requestDto);
 
             var response = await PostEntityAsync<FixtureScheduleCollectionResponse>(

d937d7e85c3eaa04ca2e7ad6c98f0b7dc9b0f1a1
Noam Mermelstein <143781297+noammerLS@users.noreply.github.com>
2025-07-06 14:39:42 +0300

    Added additional values to StatusDescription + Version Update to 1.0.3 (#60)

--- test/Trade360SDK.Common.Entities.Tests/Entities/Enums/StatusDescriptionTests.cs
+++ test/Trade360SDK.Common.Entities.Tests/Entities/Enums/StatusDescriptionTests.cs
@@ -20,6 +20,40 @@ namespace Trade360SDK.Common.Tests.Entities.Enums
             Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.Timeout));
             Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.HomeWalkover));
             Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.AwayWalkover));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.MatchDrawn));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.BallMarkInspection));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.BallMarkInspectionCompleted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.CorrectionMode));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.MedicalTreatment));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.TreatmentCompleted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.PlayersOnCourt));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.TossStarted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.TossSaved));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.UmpireOnCourt));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.UmpiringCancelled));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.WarmUpStarted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.WarmUpCompleted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.MedicalTimeoutCancelled));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.MedicalTimeoutCompleted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.SwapServer));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.SwapReceiver));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.ChallengeStarted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.ChallengeCompleted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.ChallengeCancelled));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.ToiletBreakStarted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.ToiletBreakCompleted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.ToiletBreakCancelled));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.EvaluationStarted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.TimePenalty));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.GamePenalty));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.CoachingStarted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.CoachingCancelled));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.CoachingCompleted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.Breaktime));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.PointStarted));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.PointScored));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.PointFault));
+            Assert.True(System.Enum.IsDefined(typeof(StatusDescription), StatusDescription.PointReplayed));
         }
     }
 } 

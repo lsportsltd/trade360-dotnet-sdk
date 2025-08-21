36010bb920894057ce3a34ac4340ba291c21d7ab
noammerLS <noam.m@lsports.eu>
2025-08-21 15:53:38 +0300

    Update RabbitMessageProperties.cs

--- src/Trade360SDK.Common.Entities/Models/RabbitMessageProperties.cs
+++ src/Trade360SDK.Common.Entities/Models/RabbitMessageProperties.cs
@@ -1,14 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Trade360SDK.Common.Models
 {
     public class RabbitMessageProperties
     {
+        private const string MessageGuidKey = "MessageGuid";
+        private const string MessageTypeKey = "MessageType";
+        private const string TimestampInMsKey = "timestamp_in_ms";
+        private const string MessageSequenceKey = "MessageSequence";
+        private const string FixtureIdKey = "FixtureId";
+        
         public string MessageType { get; internal set; }
         public string MessageSequence { get; internal set; }
         public string MessageGuid { get; internal set; }
         public string FixtureId { get; internal set; }
+        public string TimestampInMs { get; internal set; }
+        
 
         internal RabbitMessageProperties() { }
         
@@ -19,22 +29,33 @@ namespace Trade360SDK.Common.Models
                 throw new ArgumentNullException(nameof(properties));
             }
             
-            return new RabbitMessageProperties
+            return new RabbitMessageProperties()
             {
-                MessageType = GetRequiredProperty(properties, "MessageType"),
-                MessageSequence = GetRequiredProperty(properties, "MessageSequence"),
-                MessageGuid = GetRequiredProperty(properties, "MessageGuid"),
-                FixtureId = GetRequiredProperty(properties, "FixtureId")
+                MessageGuid = GetRequiredProperty(properties, MessageGuidKey),
+                MessageType = GetRequiredProperty(properties, MessageTypeKey),
+                TimestampInMs = GetRequiredProperty(properties, TimestampInMsKey),
+                MessageSequence = GetRequiredProperty(properties, MessageSequenceKey, false),
+                FixtureId = GetRequiredProperty(properties, FixtureIdKey, false)
             };
         }
 
-        private static string GetRequiredProperty(IDictionary<string, object> properties, string key)
+        private static string GetRequiredProperty(IDictionary<string, object> properties, string key, bool required = true)
         {
-            if (!properties.TryGetValue(key, out var value) || value == null)
+            if (properties.TryGetValue(key, out var value) && value != null)
             {
-                throw new ArgumentException($"Header '{key}' is missing or null in message properties object.", nameof(properties));
+                return value switch
+                {
+                    byte[] bytes => Encoding.UTF8.GetString(bytes),
+                    _ => value.ToString()
+                };
             }
-            return value.ToString();
+            
+            if (required)
+            {
+                throw new ArgumentException($"Header '{key}' is missing or null in message properties object.",
+                    nameof(properties));
+            }
+            return string.Empty;
         }
     }
 }

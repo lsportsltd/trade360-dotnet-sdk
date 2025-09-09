1f4e336ea7116e0b5dd3bb4691b004525499ae86
shir.b <shir.b@lsports.eu>
2025-09-09 14:16:31 +0300

    fixing GetOutrightLeaguesMarketsResponse after changing related to epic- https://lsports-data.atlassian.net/browse/TR-13680

--- test/Trade360SDK.SnapshotApi.Tests/Entities/Responses/GetOutrightLeaguesMarketsResponseTests.cs
+++ test/Trade360SDK.SnapshotApi.Tests/Entities/Responses/GetOutrightLeaguesMarketsResponseTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Trade360SDK.SnapshotApi.Entities.Responses;
 using Xunit;
+using Trade360SDK.Common.Entities.OutrightLeague;
 
 namespace Trade360SDK.SnapshotApi.Tests.Entities.Responses
 {
@@ -9,32 +10,53 @@ namespace Trade360SDK.SnapshotApi.Tests.Entities.Responses
         [Fact]
         public void CanAssignAndRetrieveAllProperties()
         {
-            var market = new OutrightMarketsResponse { FixtureId = 111, Markets = null };
-            var snapshotEvent = new SnapshotOutrightEventsResponse
+            var marketLeague = new MarketLeague { Id = 111, Name = "Market", Bets = null };
+            var snapshotEvent = new OutrightLeagueMarketEvent
             {
-                Id = 222,
-                Name = "EventName",
-                Type = 4,
-                Events = new List<OutrightMarketsResponse> { market }
+                FixtureId = 222,
+                Markets = new List<MarketLeague> { marketLeague }
             };
-            var response = new GetOutrightLeaguesMarketsResponse
+            var competitionWrapper = new OutrightLeagueCompetitionWrapper<OutrightLeagueMarketEvent>
             {
                 Id = 333,
-                Name = "LeaguesMarkets",
-                Type = 8,
-                Competitions = new List<SnapshotOutrightEventsResponse> { snapshotEvent }
+                Name = "Competition",
+                Type = 5,
+                Competitions = new List<OutrightLeagueEventsWrapper<OutrightLeagueMarketEvent>>
+                {
+                    new OutrightLeagueEventsWrapper<OutrightLeagueMarketEvent>
+                    {
+                        Id = 444,
+                        Name = "Event Wrapper",
+                        Type = 6,
+                        Events = new List<OutrightLeagueMarketEvent> { snapshotEvent }
+                    }
+                }
             };
-
-            Assert.Equal(333, response.Id);
-            Assert.Equal("LeaguesMarkets", response.Name);
-            Assert.Equal(8, response.Type);
-            Assert.NotNull(response.Competitions);
-            var evt = Assert.Single(response.Competitions);
-            Assert.Equal(222, evt.Id);
-            Assert.Equal("EventName", evt.Name);
-            Assert.Equal(4, evt.Type);
-            var marketEvt = Assert.Single(evt.Events);
-            Assert.Equal(111, marketEvt.FixtureId);
+            
+            var response = new GetOutrightLeaguesMarketsResponse
+            {
+                Competition = new List<OutrightLeagueCompetitionWrapper<OutrightLeagueMarketEvent>> { competitionWrapper }
+            };
+            
+            Assert.NotNull(response.Competition);
+            var competition = Assert.Single(response.Competition);
+            Assert.Equal(333, competition.Id);
+            Assert.Equal("Competition", competition.Name);
+            Assert.Equal(5, competition.Type);
+            
+            Assert.NotNull(competition.Competitions);
+            var eventWrapper = Assert.Single(competition.Competitions);
+            Assert.Equal(444, eventWrapper.Id);
+            Assert.Equal("Event Wrapper", eventWrapper.Name);
+            Assert.Equal(6, eventWrapper.Type);
+            
+            Assert.NotNull(eventWrapper.Events);
+            var evt = Assert.Single(eventWrapper.Events);
+            Assert.Equal(222, evt.FixtureId);
+            Assert.NotNull(evt.Markets);
+            var marketEvt = Assert.Single(evt.Markets);
+            Assert.Equal(111, marketEvt.Id);
+            Assert.Equal("Market", marketEvt.Name);
         }
     }
 } 

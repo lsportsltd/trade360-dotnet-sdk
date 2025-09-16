86a4dc6390d646ff7eecf2d6bfead1271216f107
shir.b <shir.b@lsports.eu>
2025-09-16 14:11:03 +0300

    https://lsports-data.atlassian.net/browse/TR-18917 adding safe confirmation field

--- src/Trade360SDK.Common.Entities/Entities/Livescore/CurrentIncident.cs
+++ src/Trade360SDK.Common.Entities/Entities/Livescore/CurrentIncident.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Trade360SDK.Common.Entities.Enums;
 
 namespace Trade360SDK.Common.Entities.Livescore
 {
@@ -9,5 +10,7 @@ namespace Trade360SDK.Common.Entities.Livescore
         public string? Name { get; set; }
         
         public DateTime LastUpdate { get; set; }
+        
+        public IncidentConfirmation Confirmation { get; set; }
     }
 }

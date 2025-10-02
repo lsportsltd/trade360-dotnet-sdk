7d00f730f4b0a1879bfe3236043fb6b4ac6b96b9
noammerLS <noam.m@lsports.eu>
2025-10-02 16:26:14 +0300

    Update FixtureVenue.cs
    to nullable properties

--- src/Trade360SDK.Common.Entities/Entities/Fixtures/FixtureVenue.cs
+++ src/Trade360SDK.Common.Entities/Entities/Fixtures/FixtureVenue.cs
@@ -12,8 +12,8 @@ namespace Trade360SDK.Common.Entities.Fixtures
         public CourtSurface? CourtSurfaceType { get; set; }
         public VenueEnvironment? Environment { get; set; }
         public VenueAssignment? Assignment { get; set; }
-        public IdNamePair Country { get; set; }
-        public IdNamePair State { get; set; }
-        public IdNamePair City { get; set; }
+        public IdNamePair? Country { get; set; }
+        public IdNamePair? State { get; set; }
+        public IdNamePair? City { get; set; }
     }
 }

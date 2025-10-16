390ef5abfa8bb626a8e71b563ae918b2653f5f10
noammerLS <noam.m@lsports.eu>
2025-10-16 12:32:55 +0300

    'Participants/Get' Impl

---
+++ src/Trade360SDK.CustomersApi/Entities/MetadataApi/Requests/GetParticipantsRequest.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace Trade360SDK.CustomersApi.Entities.MetadataApi.Requests
+{
+    public class GetParticipantsRequest : BaseMetadataRequest
+    {
+        public ParticipantFilter? Filter { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+    }
+    
+    public class ParticipantFilter
+    {
+        public IEnumerable<int>? Ids { get; set; }
+        public IEnumerable<int>? SportIds { get; set; }
+        public IEnumerable<int>? LocationIds { get; set; }
+        public string? Name { get; set; }
+        public int? Gender { get; set; }
+        public int? AgeCategory { get; set; }
+        public int? Type { get; set; }
+    }
+}

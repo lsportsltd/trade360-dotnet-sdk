e5294b6f21db1b8a62c48bfc659ae390691e422b
Amit Cohen <168630601+amitccohenLS@users.noreply.github.com>
2025-11-30 14:51:09 +0200

    Add SubstitutionPlayers support to Incident entity

---
+++ src/Trade360SDK.Common.Entities/Entities/Livescore/SubstitutionPlayer.cs
@@ -0,0 +1,10 @@
+namespace Trade360SDK.Common.Entities.Livescore
+{
+    public class SubstitutionPlayer
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
+
---
+++ src/Trade360SDK.Common.Entities/Entities/Livescore/SubstitutionPlayers.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Trade360SDK.Common.Entities.Livescore
+{
+    public class SubstitutionPlayers
+    {
+        public IEnumerable<SubstitutionPlayer>? Item1 { get; set; }
+
+        public IEnumerable<SubstitutionPlayer>? Item2 { get; set; }
+    }
+}
+
